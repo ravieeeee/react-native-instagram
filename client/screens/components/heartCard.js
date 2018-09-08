742d6ace9d2eea51d24c0fb86a717d78c73acabd
JY <pyeonjy97@gmail.com>
2018-09-08 11:56:20 +0900

    convert into functional component

--- client/screens/components/heartCard.js
+++ client/screens/components/heartCard.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Avatar } from 'react-native-elements';
 
-export default class HeartCard extends Component {
-  render() {
-    const likelog = this.props.likelog;
-
-    return (
-      <View style={styles.root}>
-        <Text style={styles.txts}>
-          <Text style={styles.tb}>
-            {likelog.liker_name}
-          </Text>
-          <Text>
-            님이 회원님의 게시물을 좋아합니다.
-          </Text>
-        </Text>
-        <Avatar
-          large
-          source={{uri: likelog.img}} />
-      </View>
-    );
-  }
-}
+export default props => (
+  <View style={styles.root}>
+    <Text style={styles.txts}>
+      <Text style={styles.tb}>
+        {props.likelog.liker_name}
+      </Text>
+      <Text>
+        님이 회원님의 게시물을 좋아합니다.
+      </Text>
+    </Text>
+    <Avatar
+      large
+      source={{uri: props.likelog.img}} />
+  </View>
+);
 
 const styles = StyleSheet.create({
   root: {
