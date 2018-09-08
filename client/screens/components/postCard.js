56efc49c5de41d0960424b02903efec0e9eb8633
JY <pyeonjy97@gmail.com>
2018-09-08 13:57:19 +0900

    immediately invoked function포함한 functional component로

--- client/screens/components/postCard.js
+++ client/screens/components/postCard.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {
   View,
   Text,
@@ -7,53 +7,31 @@ import {
 import { Card } from 'react-native-elements';
 import { Ionicons } from '@expo/vector-icons';
 
-
-export default class PostCard extends Component {
-  render() {
-    const post = this.props.post;
-    const idx = this.props.idx;
-
-    switch (this.props.purpose) {
-      case 'search':
-      case 'detail':
-        return (
-          <Card
-            title={post.title}
-            image={{uri: post.image}}>
-              <Text style={{ fontWeight: 'bold' }}>
-                좋아요 {post.heart}개
-              </Text>
-              <View style={{ flexDirection: 'row' }}>
-                <Text style={{ fontWeight: 'bold' }}>
-                  {post.user_name}&nbsp;
-                </Text>
-                <Text style={{ marginBottom: 10 }}>
-                  {post.content}
-                </Text>
-              </View>
-          </Card>
-        );
-      case 'list':
-        return (
-          <Card
-            title={post.title}
-            image={{uri: post.image}}>
-              <TouchableHighlight onPress={() => {this.props.onPress(idx, post.id, post.image, post.user_id)}} underlayColor={'#fff'}>
-                <Ionicons name={'md-heart'} color={this.props.isColor === true ? 'red' : 'black'} size={25} />
+export default ({purpose, post, idx, onPress, isColor}) => {
+  return (
+    <Card
+      title={post.title}
+      image={{uri: post.image}}>
+        {(() => {
+          if (purpose === 'list') {
+            return (
+              <TouchableHighlight onPress={() => {onPress(idx, post.id, post.image, post.user_id)}} underlayColor={'#fff'}>
+                <Ionicons name={'md-heart'} color={isColor === true ? 'red' : 'black'} size={25} />
               </TouchableHighlight>
-              <Text style={{ fontWeight: 'bold' }}>
-                좋아요 {post.heart}개
-              </Text>
-              <View style={{ flexDirection: 'row' }}>
-                <Text style={{ fontWeight: 'bold' }}>
-                  {post.user_name}&nbsp;
-                </Text>
-                <Text style={{ marginBottom: 10 }}>
-                  {post.content}
-                </Text>
-              </View>
-          </Card>
-        );
-    }
-  }
+            );
+          }
+        })()}
+        <Text style={{ fontWeight: 'bold' }}>
+          좋아요 {post.heart}개
+        </Text>
+        <View style={{ flexDirection: 'row' }}>
+          <Text style={{ fontWeight: 'bold' }}>
+            {post.user_name}&nbsp;
+          </Text>
+          <Text style={{ marginBottom: 10 }}>
+            {post.content}
+          </Text>
+        </View>
+    </Card>
+  );
 }
